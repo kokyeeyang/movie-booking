93eabbc4ff05bc2287f4a7c699af175342b24ccb
Kok Yee Yang <yeeyang.kok@spencer-ogden.com>
2024-10-22 14:09:38 +0800

    changed to dynamic switching of production and development frontend url

--- server/app.js
+++ server/app.js
@@ -14,13 +14,31 @@ const customerRouter = require("./routes/customerRoutes");
 
 const port = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    // origin: "http://localhost:3000",
-    origin: process.env.REACT_APP_FRONTEND_URL || "http://localhost:3000",
-    credentials: true,
-  })
-);
+const allowedOrigins = [
+  'https://bookanymovie.netlify.app', // Your production frontend URL
+  'http://localhost:3000' // Your local development URL
+];
+
+// app.use(
+//   cors({
+//     // origin: "http://localhost:3000",
+//     origin: process.env.REACT_APP_FRONTEND_URL || "http://localhost:3000",
+//     credentials: true,
+//   })
+// );
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or Postman)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true // If you need to send cookies or authentication headers
+}));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 app.use(express.urlencoded({ extended: true }));
