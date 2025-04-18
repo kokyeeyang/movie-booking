89339a369f5ae216d971f45ff12d03b25fdbfb89
Kok Yee Yang <yee_yang94@hotmail.com>
2025-04-18 20:03:03 +0800

    deleted unused codes

--- front-end/app/booking-page/page.tsx
+++
@@ -1,7 +0,0 @@
-import MovieBookingPageComponent from "components/MovieBooking/MovieBookingPage";
-
-export default function MovieBookingPage () {
-    return (
-        <MovieBookingPageComponent />
-    )
-}
