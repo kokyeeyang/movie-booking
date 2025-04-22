345d63a02a07e165ce0bba8d9dae6eed31d84d96
Kok Yee Yang <yee_yang94@hotmail.com>
2025-04-22 23:31:11 +0800

    try again

--- front-end/next.config.js
+++ front-end/next.config.js
@@ -3,8 +3,8 @@ const path = require('path')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src')
-    return config
+    config.resolve.alias['components'] = path.resolve(__dirname, 'front-end/components');
+    return config;
   }
 }
 
