234c78b0a17e6eb9a1015ab84c0614282325d544
Kok Yee Yang <yee_yang94@hotmail.com>
2025-04-22 23:16:58 +0800

    trial

--- front-end/next.config.js
+++ front-end/next.config.js
@@ -1,6 +1,11 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-};
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src')
+    return config
+  }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
