dfcee31bc960e20723afb97846407ca0ec6d2d7d
Kok Yee Yang <yee_yang94@hotmail.com>
2025-04-23 15:07:20 +0800

    fixed error

--- front-end/app/movie-booking-slots/page.tsx
+++ front-end/app/movie-booking-slots/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import MovieBookingSlotsComponent from "components/MovieBookingSlots";
 // need to use this hook instead of middleware because we are exporting this application as a static site in netlify
 import useAuthRedirect from "@/hooks/useAuthRedirect";
