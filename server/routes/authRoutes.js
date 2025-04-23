b59464c90dbd4b02acd5f72ebe7823c63de03b1f
Kok Yee Yang <yee_yang94@hotmail.com>
2025-04-24 00:13:56 +0800

    changed to using jwt_secret

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -26,7 +26,7 @@ const verifyToken = (req, res, next) => {
   if (!token) return res.status(401).json({ message: "No token provided" });
 
   try {
-    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded.user; // Attach decoded user info
     next();
   } catch (err) {
